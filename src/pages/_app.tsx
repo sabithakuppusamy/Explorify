4d18595e67a1b38ba18131aa1c153801938084e6
sabithakuppusamy <sabi95oct@gmail.com>
2022-08-22 11:23:06 +0530

    #chore- Update Head in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,12 @@
 import "../../styles/tailwind.scss";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import Layout from "./components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       </Head>
       <Component {...pageProps} />
     </>
