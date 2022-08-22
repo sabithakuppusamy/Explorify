c329622a02c0b5fcd0b3f2bef8c186fac5b9a04a
Sabitha Kuppusamy <sabi95oct@gmail.com>
2022-08-22 11:25:49 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
       </Head>
       <Component {...pageProps} />
     </>
